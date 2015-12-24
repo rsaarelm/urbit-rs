dde794b4dc59f5fb7633f9e4753708efe1ba41cd
Risto Saarelma <risto.saarelma@iki.fi>
2015-12-24 11:07:25 +0200

    Hoon parser does gaps

--- src/hoon.rs
+++ src/hoon.rs
@@ -2,15 +2,39 @@ use std::str;
 use std::str::FromStr;
 use nom::*;
 use nom::IResult::*;
+use nom::Err::*;
 use nock::Noun;
 
-// XXX: Is there a nicer way to ignore the results from eg. newline than
-// the superfluous chain!?
-named!(gap<()>, alt!(
-        chain!(newline, || { () })
-      | chain!(tag!(" ") ~ tag!(" ") ~ multispace, || { () })
-    )
-);
+/// Match at least two spaces or one newline.
+pub fn gap(input: &[u8]) -> IResult<&[u8], &[u8]> {
+    let mut spaces = 0;
+    let mut idx = 0;
+    for item in input.iter() {
+        if *item == '\t' as u8 {
+            // Physical tabs are banned.
+            // XXX: Should have own ErrorKind tag.
+            return Error(Position(ErrorKind::MultiSpace, input));
+        }
+
+        if *item == '\n' as u8 {
+            spaces += 2;
+        } else if is_space(*item) {
+            spaces += 1;
+        }
+
+        if !is_space(*item) && *item != '\r' as u8 && *item != '\n' as u8 {
+            break;
+        }
+
+        idx += 1;
+    }
+
+    if spaces < 2 {
+        return Error(Position(ErrorKind::MultiSpace, input));
+    } else {
+        return Done(&input[idx..], &input[0..idx]);
+    }
+}
 
 // TODO: Handle separator dots
 named!(atom<Noun>,
@@ -23,30 +47,31 @@ named!(atom<Noun>,
   )
 );
 
-named!(wtcl<Noun>,
-   chain!(
-       tag!("?:") ~
-       x: alt!(
-           chain!(
-               gap ~
-               p: atom ~
-               gap ~
-               q: atom ~
-               gap ~
-               r: atom,
-               || { n![6, p, q, r] })
+named!(pqr<(Noun, Noun, Noun)>,
+    alt!(
+        chain!(
+            p: preceded!(gap, atom) ~
+            q: preceded!(gap, atom) ~
+            r: preceded!(gap, atom),
+            || { (p, q, r) })
         | delimited!(
             tag!("("),
             chain!(
-               p: atom ~
-               space ~
-               q: atom ~
-               space ~
-               r: atom,
-               || { n![6, p, q, r] }),
+                p: atom ~
+                multispace ~
+                q: atom ~
+                multispace ~
+                r: atom,
+                || { (p, q, r) }),
             tag!(")"))
-        ),
-    || { x }
+    )
+);
+
+named!(wtcl<Noun>,
+   chain!(
+       tag!("?:")
+     ~ a: pqr
+     , || { let (p, q, r) = a.clone(); n![6, p, q, r] }
     )
 );
 
@@ -62,18 +87,32 @@ named!(hoon<Noun>,
 mod test {
     use nom::IResult;
     use nock::Noun;
-    use super::{gap, atom, wtcl};
+    use super::{gap, atom, pqr, wtcl};
 
     #[test]
     fn test_parse_hoon() {
-        assert_eq!(atom(&b"1234"[..]), IResult::Done(&b""[..], Noun::Atom(1234)));
+        assert_eq!(atom(&b"1234"[..]),
+                   IResult::Done(&b""[..], Noun::Atom(1234)));
 
-        assert_eq!(gap(&b"  "[..]), IResult::Done(&b""[..], ()));
-        assert_eq!(gap(&b"    "[..]), IResult::Done(&b""[..], ()));
-        assert_eq!(gap(&b"\n"[..]), IResult::Done(&b""[..], ()));
-        assert!(gap(&b" "[..]) != IResult::Done(&b""[..], ()));
+        assert!(gap(&b"  "[..]).is_done());
+        assert!(gap(&b"    "[..]).is_done());
+        assert!(gap(&b"\n"[..]).is_done());
+        assert!(gap(&b"\n  "[..]).is_done());
+        assert!(gap(&b"  \n  "[..]).is_done());
+        assert!(!gap(&b" "[..]).is_done());
 
-        assert_eq!(wtcl(&b"?:(1 2 3)"[..]), IResult::Done(&b""[..], n![6, 1, 2, 3]));
-        assert_eq!(wtcl(&b"?:  1\n  2\n3"[..]), IResult::Done(&b""[..], n![6, 1, 2, 3]));
+        assert_eq!(pqr(&b"(1 2 3)"[..]),
+                   IResult::Done(&b""[..], (Noun::Atom(1), Noun::Atom(2), Noun::Atom(3))));
+        assert_eq!(pqr(&b"\n1\n2\n3"[..]),
+                   IResult::Done(&b""[..], (Noun::Atom(1), Noun::Atom(2), Noun::Atom(3))));
+        assert_eq!(pqr(&b"  1  2  3"[..]),
+                   IResult::Done(&b""[..], (Noun::Atom(1), Noun::Atom(2), Noun::Atom(3))));
+
+        assert_eq!(wtcl(&b"?:(1 2 3)"[..]),
+                   IResult::Done(&b""[..], n![6, 1, 2, 3]));
+        assert_eq!(wtcl(&b"?:(1   2\n3)"[..]),
+                   IResult::Done(&b""[..], n![6, 1, 2, 3]));
+        assert_eq!(wtcl(&b"?:  1\n  2\n3"[..]),
+                   IResult::Done(&b""[..], n![6, 1, 2, 3]));
     }
 }
