d14fe117c4946c8b6b3d37be1f4f31ab9fe0107d
Risto Saarelma <risto.saarelma@iki.fi>
2016-01-17 08:35:33 +0200

    Use noun folding to get urbit noun atom count

--- examples/urbit.rs
+++ examples/urbit.rs
@@ -1,13 +1,17 @@
 extern crate time;
 extern crate nock;
 extern crate urbit;
+extern crate num;
 
 use std::env;
 use std::rc::Rc;
 use std::io::prelude::*;
 use std::fs::File;
 use nock::Noun::*;
+use nock::FoldState;
 use nock::nock_on;
+use num::bigint::BigUint;
+use num::traits::One;
 
 fn main() {
     let mut file = File::open(env::args().nth(1).expect("Usage scan [urbit.pill]")).unwrap();
@@ -17,7 +21,17 @@ fn main() {
     println!("Unpacking pill");
     let time_start = time::precise_time_s();
     let noun = urbit::unpack_pill(buf).unwrap();
-    println!("Unpacked pill in {:.03} seconds.", time::precise_time_s() - time_start);
+    println!("Unpacked pill in {:.03} seconds.",
+             time::precise_time_s() - time_start);
+
+    let count: BigUint = noun.fold(|x| {
+        match x {
+            FoldState::Cell(p, q) => p + q,
+            _ => One::one(),
+        }
+    });
+
+    println!("Pill has {} atoms", count);
 
     println!("Nocking pill");
     let noun = nock_on(&Atom(0), &noun).unwrap();
