448731c5b6b45b2b7e9c236fd2ef57dae3ed43a6
Risto Saarelma <risto.saarelma@iki.fi>
2016-04-03 17:37:17 +0300

    WIP boot sequence in example

--- examples/urbit.rs
+++ examples/urbit.rs
@@ -10,6 +10,15 @@ use num::traits::One;
 use nock::{Nock, Noun, Shape};
 use urbit::VM;
 
+fn get_size(noun: &Noun) -> BigUint {
+    noun.fold(|x| {
+        match x {
+            Shape::Cell(p, q) => p + q,
+            _ => One::one(),
+        }
+    })
+}
+
 fn main() {
     let mut file = File::open(env::args()
                                   .nth(1)
@@ -19,21 +28,30 @@ fn main() {
     let mut buf = Vec::new();
     file.read_to_end(&mut buf).unwrap();
     println!("Unpacking pill");
-    let noun = urbit::unpack_pill(buf).unwrap();
+    let pill = urbit::unpack_pill(buf).unwrap();
+
+    // You can't nock the whole pill as an
+    let (kernel, arvo) = match pill.get() {
+        Shape::Cell(kernel, arvo) => ((*kernel).clone(), (*arvo).clone()),
+        _ => panic!("bad"),
+    };
     println!("Unpacked pill");
 
-    let count: BigUint = noun.fold(|x| {
-        match x {
-            Shape::Cell(p, q) => p + q,
-            _ => One::one(),
-        }
-    });
+    println!("Kernel has {} atoms", get_size(&kernel));
+    println!("Arvo has {} atoms", get_size(&arvo));
 
-    println!("Pill has {} atoms", count);
-
-    println!("Nocking pill");
     let mut vm = VM::new();
-    let noun = vm.nock_on(Noun::from(0u32), noun);
+
+    println!("Nocking kernel");
+    let noun = vm.nock_on(Noun::from(0u32), kernel).unwrap();
     vm.print_status();
-    println!("Result: {:?}", noun);
+    println!("Kernel nocked, subject has {} atoms", get_size(&noun));
+    println!("{}", noun);
+
+    // FIXME: This isn't the right way to boot...
+    println!("Nocking arvo");
+    let noun = vm.nock_on(Noun::from(0u32), arvo).unwrap();
+    vm.print_status();
+    println!("Arvo nocked, subject has {} atoms", get_size(&noun));
+    println!("{}", noun);
 }
